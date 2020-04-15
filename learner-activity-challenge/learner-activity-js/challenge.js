5ee2e2b0866d76476b5b6c217202974366671565
Azra Efendic <azra_efendic@live.com>
2020-04-15 13:06:40 +0200

    Finished Learner Activity Challenge

--- learner-activity-challenge/learner-activity-js/challenge.js
+++ learner-activity-challenge/learner-activity-js/challenge.js
@@ -1,11 +1,50 @@
+function evaluateLearnerActivity(activityArray) {
+    let activitySummary = [];
 
- function evaluateLearnerActivity(activityArray) {
+    // calculate the number of days for which the user completed a lesson
+    let completedLessons = 0;
+    activityArray.forEach(day => {
+        if (day === 1) {
+            completedLessons++;
+        }
+    });
+    activitySummary.push(completedLessons);
 
-  let activitySummary = [];
+    // calculate the user's longest streak
+    let longestStreak = 0;
+    let currentStreak = 0;
+    activityArray.map(day => {
+        if (day === 1) {
+            currentStreak++;
+        } else {
+            // current streak finished, compare with longest streak
+            if (currentStreak > longestStreak) {
+                // new longest streak found!
+                longestStreak = currentStreak;
+            }
+            // reset current streak for use in next iterations
+            currentStreak = 0;
+        }
+    });
+    activitySummary.push(longestStreak);
 
-  //   Your code here
+    // calculate the user's learning status based on last 7 days
+    let learnerStatus;
+    let status = 0;
+    // create new array containing last 7 days (or less if array's length < 7)
+    let lastSevenDays = activityArray.slice(-7);
+    lastSevenDays.forEach(day => (status += day));
 
-  return activitySummary;
+    if (status <= 2) {
+        learnerStatus = "Slow starter";
+    } else if (status <= 4) {
+        learnerStatus = "Getting there";
+    } else if (status > 4) {
+        learnerStatus = "Super user";
+    }
+    activitySummary.push(learnerStatus);
+
+    return activitySummary;
 }
 
 // do not remove or change name
